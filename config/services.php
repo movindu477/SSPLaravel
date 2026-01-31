f9af966cc93b99f480173f3e7ac39be892bb67c2
movindu477 <peshalaweerabahu0@gmail.com>
2026-01-31 11:58:30 +0530

    Standardize Stripe configuration to use config() and exact env keys

--- config/services.php
+++ config/services.php
@@ -36,8 +36,8 @@ return [
     ],
 
     'stripe' => [
-        'secret' => env('STRIPE_SECRET_KEY'),
-        'public' => env('STRIPE_PUBLIC_KEY'),
+        'secret' => env('STRIPE_SECRET'),
+        'key' => env('STRIPE_KEY'),
     ],
 
 ];
